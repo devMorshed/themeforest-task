ade09cce7f5eb94bfc159756646775d5f17f7129
devMorshed <ac3m0r@gmail.com>
2023-07-17 14:12:33 +0600

    Slider initialized

---
+++ assets/js/theme.js
@@ -0,0 +1,15 @@
+(function ($) {
+	"use strict";
+
+	$(document).ready(function () {
+		$(".owl-carousel").owlCarousel({
+			animateOut: "slideOutDown",
+			items: 1,
+			loop: true,
+			responsiveClass: true,
+			autoplay: true,
+			autoplayTimeout: 5000,
+			// autoplayHoverPause: true,
+		});
+	});
+})(jQuery);
